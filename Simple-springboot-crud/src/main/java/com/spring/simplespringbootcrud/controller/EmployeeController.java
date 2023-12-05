76cbe8be0b5a19200c815156159e5610e366d669
samidisjg <samidisjg@gmail.com>
2023-12-06 02:06:36 +0530

    Delete Spring Employee compeletion

--- Simple-springboot-crud/src/main/java/com/spring/simplespringbootcrud/controller/EmployeeController.java
+++ Simple-springboot-crud/src/main/java/com/spring/simplespringbootcrud/controller/EmployeeController.java
@@ -4,6 +4,7 @@ import com.spring.simplespringbootcrud.exception.ResourceNotFoundException;
 import com.spring.simplespringbootcrud.model.Employee;
 import com.spring.simplespringbootcrud.repository.EmployeeRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -51,6 +52,17 @@ public class EmployeeController {
 
         employeeRepository.save(updateEmployee);
         return ResponseEntity.ok(updateEmployee);
+    }
+
+
+    //build delete employee rest api
+    @DeleteMapping("/employees/{id}")
+    public ResponseEntity<HttpStatus> deleteEmployee(@PathVariable long id){
+        Employee employee = employeeRepository.findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException("Employee not exist with id: " + id));
+
+        employeeRepository.delete(employee);
+        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
     }
 
 }
